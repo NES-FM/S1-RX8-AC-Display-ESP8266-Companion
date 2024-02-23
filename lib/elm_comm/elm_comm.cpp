c32df974e279fb85bbf1b6e9f385b0c08ff31f99
Florian Magg <florian.magg@outlook.com>
2024-02-23 14:26:09 +0100

    changed debug

--- lib/elm_comm/elm_comm.cpp
+++ lib/elm_comm/elm_comm.cpp
@@ -7,17 +7,17 @@ void elm_comm::init() {
     elm = new ELM327();
 
     hasInitializedElm = false;
+
+    Serial.println(
+        "Voltage,load,coolantTemp,rpm,kph,timingAdvance,intakeAirTemp,throttle,fuelLevel,absLoad,commandedAirFuelRatio,"
+        "ambientAirTemp,fuelType,ethanolPercent,oilTemp,demandedTorque,torque,referenceTorque,deltaTime" );
 }
 
 void elm_comm::tick() {
-    if ( millis() - lastLoop > 50 ) {
-        if ( !hasInitializedElm ) {
-            initializeElm();
-            return;
-        }
-
+    if ( hasInitializedElm && millis() - lastLoop > 50 ) {
         unsigned long a = millis();
 
+        float volt = elm->batteryVoltage();
         float load = elm->engineLoad();
         float coolantTemp = elm->engineCoolantTemp();
         float rpm = elm->rpm();
@@ -36,9 +36,7 @@ void elm_comm::tick() {
         float torque = elm->torque();
         uint16_t referenceTorque = elm->referenceTorque();
 
-        Serial.printf( "load:%s,coolantTemp:%s,rpm:%s,kph:%d,timingAdvance:%s,intakeAirTemp:%s,throttle:%s,"
-                       "fuelLevel:%s,absLoad:%s,commandedAirFuelRatio:%s,ambientAirTemp:%s,fuelType:%d,"
-                       "ethanolPercent:%s,oilTemp:%s,demandedTorque:%s,torque:%s,referenceTorque:%d,deltaTime:%lu\r\n",
+        Serial.printf( "%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s,%d,%lu\r\n", String( volt ).c_str(),
                        String( load ).c_str(), String( coolantTemp ).c_str(), String( rpm ).c_str(), (int)kph,
                        String( timingAdvance ).c_str(), String( intakeAirTemp ).c_str(), String( throttle ).c_str(),
                        String( fuelLevel ).c_str(), String( absLoad ).c_str(), String( commandedAirFuelRatio ).c_str(),
